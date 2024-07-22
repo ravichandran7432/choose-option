d08c660bd482ac2ae52470211c08bb05a5efafd9
ravichandran7432 <112681001+ravichandran7432@users.noreply.github.com>
2024-07-22 16:31:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function myfunc1(){
+    document.getElementById("a1").style.backgroundColor="red";
+}
+function myfunc2(){
+    document.getElementById("a2").style.backgroundColor="red";
+}
+function myfunc3(){
+    document.getElementById("a3").style.backgroundColor="red";
+}
+function myfunc4(){
+    document.getElementById("a4").style.backgroundColor="green";
+}
